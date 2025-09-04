1236e0b3d8b38e323dc9d108cf663a85cdfaeadc
Yousef10 <yousef_espetan@yahoo.com>
2025-09-05 01:51:46 +0300

    installing basic packages for codemirror 6

--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
 // Required imports
 import { EditorState } from '@codemirror/state';
-import { basicSetup } from "codemirror";
 import { EditorView } from '@codemirror/view';
+import { basicSetup } from "codemirror";
 
+import { javascript } from '@codemirror/lang-javascript';
+import { oneDark } from '@codemirror/theme-one-dark';
+
+import { takeScreenshot } from './screenshot.js';
+
+
+
+const initialCode = `// Welcome to the CodeMirror 6 Screenshot Tool!
+// 1. Select a block of code you want to capture.
+// 2. Click the "Take Screenshot" button.
+// 3. The generated image will appear below.
+
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+const result = fibonacci(10);
+console.log(\`Fibonacci(10) is \${result}\`);
+
+class Greeter {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greet() {
+    return \`Hello, \${this.name}!\`;
+  }
+}
+`;
